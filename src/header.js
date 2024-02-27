8e6561a156e2ea548bdcd008fd4b2a7eb9757741
mayorwise001 <tomosorijoseph@yahoo.com>
2024-02-27 22:49:16 +0100

    Added the top header file

--- src/header.js
+++ src/header.js
@@ -5,12 +5,18 @@ function topSection() {
     let topimage = document.createElement('div');
     topimage.classList.add('about-image');
 
+    //Image File
     let myimag = document.createElement('img')
     myimag.src = myimage
     myimag.classList.add('my-image');
+    let mynametitle = document.createElement('h2');
+    mynametitle.innerHTML = "Tomosori Joseph Mayowa";
+    mynametitle.classList.add('my-name');
 
-  topimage.appendChild(myimag)
-  topcontent.appendChild(topimage)
+    topimage.appendChild(myimag)
+    topimage.appendChild(mynametitle)
+    topcontent.appendChild(topimage)
+
     return topcontent;
   }
   
